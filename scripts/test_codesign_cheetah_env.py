504d3cd17cfc6be854e0b43f6aa4b7cdb63804ee
Varun Madabushi <vmadabushi3@gatech.edu>
2026-05-25 18:08:43 -0400

    Generating new models on CPU side since the mjspec compile is not compatible with jax

--- scripts/test_codesign_cheetah_env.py
+++ scripts/test_codesign_cheetah_env.py
@@ -1,10 +1,11 @@
 from codesign.envs.CodesignCheetah import CodesignCheetah
+from codesign.envs import CodesignCheetah
 import moplayground as mop
-from codesign.eval import rollout_policy
 import minimal_mjx as mm
 import mujoco
 import argparse
 import jax.numpy as jnp
+import numpy as np
 from moplayground.envs.dmcontrol.cheetah import MOCheetah
 from pathlib import Path
 
@@ -12,24 +13,24 @@ from pathlib import Path
 CONFIG_PATH = 'config/codesign_cheetah.yaml'
 train_config = mop.utils.read_config(CONFIG_PATH)
 env_params = mm.utils.config.create_config_dict(train_config['env_config'])
-env = CodesignCheetah(env_params=env_params, backend="np")
+ds = np.linspace(0.5, 2, 5)
+envs = [CodesignCheetah.generate_model(env_params, 'np', d) for d in ds]
 
-# env.set_mj_model(env.generate_model(1))
+for d, env in zip(ds, envs):
+    def do_nothing(obs, rng):
+        return jnp.zeros(env.action_size), 0.0
 
-def do_nothing(obs, rng):
-    return jnp.zeros(env.action_size), 0.0
+    frames, reward_plotter, _, _ = mm.eval.rollout_policy(
+            inference_fn    = do_nothing,
+            env             = env,
+            T               = 2,
+            width           = 640,
+            height          = 480,
+            camera          = 'track',
+        )
 
-frames, reward_plotter, _, _ = rollout_policy(
-        inference_fn    = do_nothing,
-        env             = env,
-        T               = 2,
-        width           = 640,
-        height          = 480,
-        camera          = 'track',
+    mm.utils.plotting.save_video(
+        frames,
+        env.dt,
+        Path(f'output/videos/{train_config['env']}-{d}-rollout.mp4')
     )
-
-mm.utils.plotting.save_video(
-    frames,
-    env.dt,
-    Path(f'output/videos/{train_config['env']}-rollout.mp4')
-)
--- src/codesign/envs/CodesignCheetah.py
+++ src/codesign/envs/CodesignCheetah.py
@@ -10,84 +10,40 @@ from mujoco_playground._src import mjx_env
 from codesign.envs import CodesignInterface
 from moplayground.envs.dmcontrol.interface import CheetahInterface
 from moplayground.envs.dmcontrol.cheetah import MOCheetah
+from mujoco_playground._src.dm_control_suite import common
+import mujoco as mj
 import jax.numpy as jnp
+import numpy as np
 from pathlib import Path
 
 INTERFACE_PATH = Path(__file__).resolve().parent
-class CodesignCheetah(MOCheetah, CodesignInterface.CodesignInterface):
-    # Implements a swappable leg length for MOCheetah
-    # CodesignInterface forces modify_design and resample_design to be implemented
-
-    def __init__(self, env_params, backend):
+class CodesignCheetah(MOCheetah):
+    def __init__(self, env_params, backend, new_model):
         super().__init__(env_params, backend, xml_path=INTERFACE_PATH / "cheetah.xml")
+        self._mj_model = new_model
+        self.setup_swappable_backend(self._backend) # Once model is updated, data_init_fn also needs to be updated
 
-    def generate_model(self, d):
-        shin_pos = jnp.array([0.2, 0, -0.26])
-        new_shin_pos = shin_pos*d
-        midpoint = new_shin_pos/2
-
-        # load spec from file
-        spec = self._spec
-
-        spec.body("bshin").pos = new_shin_pos
-        thigh_geom = spec.geom("bthigh")
-        thigh_geom.pos = midpoint  # Change to desired position (x, y, z)
-        thigh_geom.size[1] = jnp.linalg.norm(new_shin_pos)/2
-        return spec.compile() # This may need to be swappable backended to return either a mujoco model or mjx model
-
-    def resample_design(self, rng):
-        length = self._uniform(rng, shape=(1,), minval=0.5, maxval=2)
-        return length
-    
-    def set_mj_model(self, model):
-        self._mj_model = model
-
-    # Randomizes leg length upon reset
-    def reset(self, rng: jax.Array) -> mjx_env.State:
-        rng, leg_length_key  = self._split(rng, 2)
-
-        # Checks params dict to see if design should be resampled, and if so, how
-        codesign = self.params.codesign
-        if(codesign.enabled):
-            if(codesign.strategy == 'random'):
-                d = self.resample_design(leg_length_key)
-            elif(codesign.strategy == 'predictor'):
-                # This is where the design predictor would go
-                raise NotImplementedError()
-        else:
-            d = 1
-
-        new_model = self.generate_model(d)
-        parent_state = super().reset(rng)
-        info = {}
-        info['design'] = d
-        info['model'] = new_model
-        parent_state.info = parent_state.info | info # Can you do this?
-
-        return parent_state
-
-    def step(self, state, action):
-        state.info['xposbefore'] = state.data.qpos[0]
-        action = self._np.clip(
-            self.params.action_scale * action, 
-            -1.0,
-            1.0
+    @classmethod
+    def default_spec(cls) -> mj.MjSpec:
+        return mj.MjSpec.from_file(
+            filename=Path(INTERFACE_PATH / "cheetah.xml").as_posix(), 
+            assets=common.get_assets()
         )
-        data = self._step_fn(state.data, action, model=state.info['model'])
-        state.info['xposafter'] = data.qpos[0]
-        state.info['ang']       = data.qpos[2]
-        
-        done = self.fall_termination(state.info)
-        rewards = self.reward_function(
-            data   = data,
-            action = action,
-            info   = state.info,
-            done   = done
-        )
-        reward, metrics = self.get_reward_and_metrics(rewards, state.metrics)
-        obs = self._get_obs(
-            data,
-            state.info
-        )
-        done = done.astype(float)
-        return self._state_init_fn(data, obs, reward, done, metrics, state.info)
+
+def generate_model(env_params, backend, d):
+    shin_pos = jnp.array([0.2, 0, -0.26])
+    new_shin_pos = shin_pos*d
+    midpoint = new_shin_pos/2
+
+    spec = CodesignCheetah.default_spec()
+
+    # load spec from file
+    spec.body("bshin").pos = new_shin_pos
+    thigh_geom = spec.geom("bthigh")
+    thigh_geom.pos = midpoint  # Change to desired position (x, y, z)
+    thigh_geom.size[1] = jnp.linalg.norm(new_shin_pos)/2
+    return CodesignCheetah(env_params, backend, spec.compile())
+
+def resample_design(rng):
+    length = np.random.uniform(shape=(1,), minval=0.5, maxval=2)
+    return length
