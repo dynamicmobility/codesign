b9c3c8c311312a39902cfeaced298e09537c3658
njanwani <njanwani@gatech.edu>
2026-07-17 02:50:53 -0400

    remove double inheritance for cheetah

--- src/codesign/envs/CodesignBase.py
+++ src/codesign/envs/CodesignBase.py
@@ -9,6 +9,7 @@ from mujoco import mjx
 from mujoco_playground._src import mjx_env
 
 from minimal_mjx.envs import SwappableBase
+import moplayground as mop
 from mujoco_playground._src.dm_control_suite import common
 import mujoco as mj
 import jax.numpy as jnp
@@ -37,7 +38,7 @@ class CodesignBase(SwappableBase):
     @abstractmethod
     def generate_model(cls, d):
         pass
-        
+    
     @classmethod
     def default_spec(cls, xml_path: Path) -> mj.MjSpec:
         return mj.MjSpec.from_file(
@@ -56,6 +57,44 @@ class CodesignBase(SwappableBase):
         if isinstance(obs, dict):
             return jax.tree_util.tree_map(lambda x: x.shape, obs)
         return obs.shape[-1]
+    
+    @property
+    @abstractmethod
+    def design_limits(self):
+        """Returns a _np array [low, high], where low/high are the same shape as
+        the design parameter d."""
+        raise NotImplementedError()
+    
+    
+class MOCodesignBase(CodesignBase):
+    
+    def __init__(
+        self, 
+        xml_path,
+        env_params, 
+        backend, 
+        num_free,
+        mo_backend=None
+    ):
+        super().__init__(
+            xml_path    = xml_path,
+            env_params  = env_params,
+            backend     = backend,
+            num_free    = num_free
+        )
+        
+        if mo_backend is None:
+            self.mo_backend = mop.envs.generic.MultiObjectiveBase(
+                xml_path    = xml_path,
+                env_params  = env_params,
+                backend     = backend,
+                num_free    = num_free
+            )
+        else:
+            self.mo_backend: mop.envs.generic.MultiObjectiveBase = mo_backend
+    
+    def get_reward_and_metrics(self, rewards, metrics):
+        return self.mo_backend.get_reward_and_metrics(rewards, metrics)
 
 
 class CodesignMO2SO:
