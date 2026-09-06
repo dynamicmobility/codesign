11b80b802e98e21f63cb2f7f99deb8c6b0cbc7bf
Varun Madabushi <vmadabushi3@gatech.edu>
2026-09-06 18:08:31 -0400

    fixed rollout_video for design mlp

--- src/codesign/learning/__init__.py
+++ src/codesign/learning/__init__.py
@@ -10,6 +10,7 @@ from codesign.learning.inference import (
     load_design_lookup_hypernetwork,
     load_mo_design_hypernetwork,
     load_mo_design_predictor_hypernetwork,
+    load_design_mlp,
 )
 
 __all__ = [
@@ -18,4 +19,5 @@ __all__ = [
     "load_design_lookup_hypernetwork",
     "load_mo_design_hypernetwork",
     "load_mo_design_predictor_hypernetwork",
+    "load_design_mlp",
 ]
